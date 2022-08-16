a05dbe95866e9ae240c6e6d452d9fda76ca06c18
Michael Puehringer <michael.puehringer@datavisyn.io>
2022-08-16 09:29:47 +0200

    Update copy command

--- bin/commands/copy.js
+++ bin/commands/copy.js
@@ -4,11 +4,9 @@ module.exports = {
   command: 'copy',
   describe: 'Copy assets, styles, and static files to the dist folder',
   handler: () => {
-    call(
-      '',
-      'if [ -d src/assets ]; then cp -rv src/assets/. dist/assets/; fi && if [ -d src/template ]; then shx --verbose cp -R src/template/. dist/template/; fi',
-    );
-    call('', 'if [ -d src/scss ]; then shx --verbose cp -R src/scss/. dist/scss/; fi');
-    call('', 'shx --verbose cp "src/*.{txt,html,ejs,json}" dist/ || echo \'no file copied\'');
+    call('shx', "--verbose cp -r src/assets/. dist/assets/ || echo 'no assets copied'");
+    call('shx', "--verbose cp -R src/template/. dist/template/ || echo 'no templates copied'");
+    call('shx', "--verbose cp -R src/scss/. dist/scss/ || echo 'no scss files copied'");
+    call('shx', '--verbose cp "src/*.{txt,html,ejs,json}" dist/ || echo \'no file copied\'');
   },
 };
