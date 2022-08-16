03b83cc181b4b1ef54370289162e7826aad3fbc7
Michael Puehringer <michael.puehringer@datavisyn.io>
2022-08-16 15:49:25 +0200

    Add src/templates to copy command

--- bin/commands/copy.js
+++ bin/commands/copy.js
@@ -5,7 +5,8 @@ module.exports = {
   describe: 'Copy assets, styles, and static files to the dist folder',
   handler: () => {
     call('shx', "--verbose cp -r src/assets/. dist/assets/ || echo 'no assets copied'");
-    call('shx', "--verbose cp -R src/template/. dist/template/ || echo 'no templates copied'");
+    call('shx', "--verbose cp -R src/template/. dist/template/ || echo 'no template copied'");
+    call('shx', "--verbose cp -R src/templates/. dist/templates/ || echo 'no templates copied'");
     call('shx', "--verbose cp -R src/scss/. dist/scss/ || echo 'no scss files copied'");
     call('shx', '--verbose cp "src/*.{txt,html,ejs,json}" dist/ || echo \'no file copied\'');
   },
