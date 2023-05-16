420cad444062e5321c25c22077ccb93d7058bfa3
Michael Puehringer <michael.puehringer@datavisyn.io>
2023-05-16 11:48:41 +0200

    Change to cypress/react18

--- tests_fixtures/standalone_template/cypress/component/component-health-check.cy.tsx
+++ tests_fixtures/standalone_template/cypress/component/component-health-check.cy.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { mount } from 'cypress/react';
 import { MainApp } from '../../src/app/MainApp';
 
 describe('Health check for Cypress component test', () => {
   it('should mount MainApp', () => {
-    mount(<MainApp />);
+    cy.mount(<MainApp />);
     cy.get('div').should('include.text', 'hello world');
   });
 });
