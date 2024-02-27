33d63a440735f89cb875e871a506d588653656d9
Michael Puehringer <michael.puehringer@datavisyn.io>
2024-02-27 15:12:35 +0100

    Switch to js cypress template

--- config/cypress.config.template.js
+++ config/cypress.config.template.js
@@ -1,24 +1,21 @@
-import { defineConfig } from 'cypress';
-import * as rspackConfig from './rspack.config';
-import { devServer } from 'cypress-rspack-dev-server';
+const { devServer } = require('cypress-rspack-dev-server');
+const rspackConfig = require('./rspack.config');
 
-export default defineConfig({
+module.exports = {
   viewportHeight: 1080,
   viewportWidth: 1920,
   defaultCommandTimeout: 10000,
   e2e: {
     baseUrl: 'http://localhost:8080',
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {},
+    setupNodeEvents() {},
   },
   component: {
     devServer(devServerConfig) {
       return devServer({
         ...devServerConfig,
         framework: 'react',
-        // @ts-ignore
         rspackConfig: rspackConfig({ workspace_mode: 'single' }, { mode: 'production' }),
       });
     },
   },
-});
+};
