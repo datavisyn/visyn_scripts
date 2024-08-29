6aed8b9cdca30f426512a7a683251a933a78de3f
Michael Puehringer <michael.puehringer@datavisyn.io>
2024-08-29 08:57:37 +0200

    Fix proxy type

--- config/rspack.config.js
+++ config/rspack.config.js
@@ -114,23 +114,22 @@ module.exports = (webpackEnv, argv) => {
         open: true,
         // Needs to be enabled to make SPAs work: https://stackoverflow.com/questions/31945763/how-to-tell-webpack-dev-server-to-serve-index-html-for-any-route
         historyApiFallback: historyApiFallback == null ? true : historyApiFallback,
-        proxy: {
+        proxy: [
           // Append on top to allow overriding /api/v1/ for example
-          ...(devServerProxy || {}),
-          ...{
-            '/api/*': {
-              target: 'http://localhost:9000',
-              secure: false,
-              ws: true,
-              // Explicitly forward close events for properly closing SSE (server-side events). See https://github.com/webpack/webpack-dev-server/issues/2769#issuecomment-1517290190
-              onProxyReq: (proxyReq, req, res) => {
-                res.on('close', () => proxyReq.destroy());
-              },
+          ...(devServerProxy || []),
+          {
+            context: ['/api/'],
+            target: 'http://localhost:9000',
+            secure: false,
+            ws: true,
+            // Explicitly forward close events for properly closing SSE (server-side events). See https://github.com/webpack/webpack-dev-server/issues/2769#issuecomment-1517290190
+            onProxyReq: (proxyReq, req, res) => {
+              res.on('close', () => proxyReq.destroy());
             },
-            // Append on bottom to allow override of exact key matches like /api/*
-            ...(devServerProxy || {}),
           },
-        },
+          // Append on bottom to allow override of exact key matches like /api/*
+          ...(devServerProxy || []),
+        ],
         client: {
           // Do not show the full-page error overlay
           overlay: false,
