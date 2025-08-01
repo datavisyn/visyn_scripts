fa7fdf254c051c663fd21badd28ab42c2f214594
dvdanielamoitzi <57343176+dvdanielamoitzi@users.noreply.github.com>
2025-08-01 17:48:53 +0200

    Fix: Add rsbuild polyfill plugin (#148)
    Closes https://github.com/datavisyn/visyn_pro/issues/395
    
    ### Developer Checklist (Definition of Done)
    
    **Issue**
    
    - [ ] All acceptance criteria from the issue are met
    - [ ] Tested in latest Chrome/Firefox
    
    **UI/UX/Vis**
    
    - [ ] Requires UI/UX/Vis review
      - [ ] Reviewer(s) are notified (_tag assignees_)
      - [ ] Review has occurred (_link to notes_)
      - [ ] Feedback is included in this PR
      - [ ] Reviewer(s) approve of concept and design
    
    **Code**
    
    - [x] Branch is up-to-date with the branch to be merged with, i.e.,
    develop
    - [ ] Code is cleaned up and formatted
    - [ ] Unit tests are written (frontend/backend if applicable)
    - [ ] Integration tests are written (if applicable)
    
    **PR**
    
    - [x] Descriptive title for this pull request is provided (will be used
    for release notes later)
    - [x] Reviewer and assignees are defined
    - [ ] Add type label (e.g., *bug*, *feature*) to this pull request
    - [x] Add release label (e.g., `release: minor`) to this PR following
    [semver](https://semver.org/)
    - [x] The PR is connected to the corresponding issue (via `Closes #...`)
    - [x] [Summary of changes](#summary-of-changes) is written
    
    
    ### Summary of changes
    
    - added @rsbuild/plugin-node-polyfill to fix
    https://github.com/datavisyn/visyn_pro/issues/395
    
    ### Screenshots
    
    
    ### Additional notes for the reviewer(s)
    
    -  
    Thanks for creating this pull request 🤗

--- config/storybook.rsbuild.config.ts
+++ config/storybook.rsbuild.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from '@rsbuild/core';
+import { pluginNodePolyfill } from '@rsbuild/plugin-node-polyfill';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
 
 // This is the rsbuild configuration used by storybook, it is not the same as the rspack config!
 export default defineConfig({
-  plugins: [pluginReact(), pluginSass()],
+  plugins: [pluginReact(), pluginSass(), pluginNodePolyfill()],
 });
